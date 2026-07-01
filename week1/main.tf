2e1783900d68b70558344b91a4b6463d4e0f964d
Kurtes <kurtesallen@gmail.com>
2026-07-01 01:14:03 -0400

    Generate failing plan.json for PR2

--- week1/main.tf
+++ week1/main.tf
@@ -59,7 +59,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "primary" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "AWS:KMS"
     }
   }
 }
